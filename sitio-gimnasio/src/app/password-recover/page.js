2f876d5bb6099fe052c6139e3fd2bccffd6fd5de
Kosmo <vladiotero19101916@gmail.com>
2024-04-22 13:54:17 -0400

    Basic routes

---
+++ sitio-gimnasio/src/app/password-recover/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PasswordRecover() {
+  return (
+    <div>PasswordRecover</div>
+  )
+}
+
+export default PasswordRecover
---
+++ sitio-gimnasio/src/app/trainers/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Trainers() {
+  return (
+    <div>
+        Trainers
+        En esta pagina se mostraran filtrados los usuarios con rol entrenador. Se podran filtrar por gimnasio. 
+        Esta pagina es accesible por todos los usuarios. La vista debe ser similar a /user/list
+    </div>
+  )
+}
+
+export default Trainers
