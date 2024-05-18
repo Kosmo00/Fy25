b151c29541b2cae341e0213a7ac0efc050d49c8f
jhuliangr <jhpiano9731@gmail.com>
2024-05-17 22:54:13 -0400

    [ADD] Scanner component

--- sitio-gimnasio/src/app/trainers/page.jsx
+++ sitio-gimnasio/src/app/trainers/page.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+// import { Html5QrcodeScanner } from 'html5-qrcode'
+import QrScanner from '@/components/Scanner'
+
 
 function Trainers() {
+  useEffect(() => {
+
+  }, [])
+
   return (
     <div>
-        Trainers
-        En esta pagina se mostraran filtrados los usuarios con rol entrenador. Se podran filtrar por gimnasio. 
-        Esta pagina es accesible por todos los usuarios. La vista debe ser similar a /user/list
+      Trainers
+      <div className='w-[50%]'>
+        <QrScanner />
+      </div>
+      En esta pagina se mostraran filtrados los usuarios con rol entrenador. Se podran filtrar por gimnasio.
+      Esta pagina es accesible por todos los usuarios. La vista debe ser similar a /user/list
     </div>
   )
 }
