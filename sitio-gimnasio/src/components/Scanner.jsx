bca2fbeb3b96cd14a93d7d33868071974470dafb
jhuliangr <jhpiano9731@gmail.com>
2024-05-26 10:32:47 -0400

    [ADD]Scanner and widths issues on profile page corrected

--- sitio-gimnasio/src/components/Scanner.jsx
+++ sitio-gimnasio/src/components/Scanner.jsx
@@ -1,35 +1,77 @@
-import { Scanner } from '@yudiel/react-qr-scanner';
-import React, { useState } from 'react';
+"use client"
+import React, { useEffect, useRef, useState } from 'react';
+import QrScanner from 'qr-scanner';
 
-const QrScanner = () => {
+const QrScannerComponent = () => {
 
-    const [state, setState] = useState(null);
-    const handleScanDone = (text, result) => {
-        console.log(state === "m" ? "Musculatura" : "Spinning" + text, result)
-        setState(null)
+  const [state, setState] = useState(null);
+  const videoRef = useRef(null);
+  const [result, setResult] = useState('');
+
+  function onResult(data) {
+    setResult(data);
+    setState(null);
+  }
+
+  useEffect(() => {
+    if (state) {
+
+      const qrScanner = new QrScanner(
+        videoRef.current,
+        (result) => onResult(result.data),
+        {
+          returnDetailedScanResult: true,
+          onDecodeError: (error) => console.error(error),
+          maxScansPerSecond: 1,
+          highlightScanRegion: true,
+          highlightCodeOutline: true,
+          preferredCamera: "user"
+        }
+      );
+      const checkCameraPermission = async () => {
+       
+  
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+          stream.getTracks().forEach(track => track.stop());
+          qrScanner.start();
+        } catch (err) {
+          setError('Camera permission denied or not available');
+          console.error(err);
+        }
+      };
+
+      checkCameraPermission();
+      qrScanner.start();
+
+      return () => {
+        qrScanner.stop();
+      };
     }
-    return (
-        <div className='w-full h-full flex justify-between'>
-            <button className='bg-black rounded-md p-3 text-white'
-                onClick={() => setState(state ? null : "M")}
-            >
-                Musculacion
-            </button>
-            <button className=' bg-black rounded-md p-3 text-white'
-                onClick={() => setState(state ? null : "S")}
-            >
-                Spinning
-            </button>
-            {
-                state &&
-                <div className='absolute top-[10%] left-[30%] w-[40%]'>
-                    <Scanner
-                        onResult={handleScanDone}
-                        onError={(error) => console.log(error?.message)}
-                    />
-                </div>
-            }
+  }, [state]);
+  return (
+    <div className='w-full h-full flex justify-between'>
+      <button className='bg-black rounded-md p-3 text-white'
+        onClick={() => setState(state ? null : "M")}
+      >
+        Musculacion
+      </button>
+      <p className='text-4xl text-[red]' hidden={result.length === 0}>{result}</p>
+      <button className=' bg-black rounded-md p-3 text-white'
+        onClick={() => setState(state ? null : "S")}
+      >
+        Spinning
+      </button>
+      {
+        state &&
+        <div className='absolute top-[10%] left-[30%] w-[40%]'>
+          <div className='bg-[green]'>
+            <h1>QR Scanner</h1>
+            <video ref={videoRef} style={{ width: '100%' }}></video>
+          </div>
         </div>
-    );
+      }
+    </div>
+  );
 }
-export default QrScanner
+export default QrScannerComponent
--- sitio-gimnasio/src/components/userProfile/LeftSide.jsx
+++ sitio-gimnasio/src/components/userProfile/LeftSide.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 function LeftSide({user, role}) {
   return (
     <>
-    <div className='sm:pl-[25%] mt-10'>
+    <div className='px-10 mt-10'>
         <div className='flex'>
             <div className='block pr-5'>
             <Image src={user.profile_image} alt="Foto de perfil" className='rounded-lg border-dashed border-[5px]' width={200} height={200} />
@@ -17,15 +17,15 @@ function LeftSide({user, role}) {
             </div>
         </div>
         <div className='mt-10 md:text-2xl text-lg'>
-            <input className='text-black text-xl p-2 bg-white sm:w-[350px] w-[323px] rounded-md' type='email' value={user.email} disabled/>
+            <input className='text-black text-xl p-2 bg-white w-full rounded-md' type='email' value={user.email} disabled/>
         </div>
         <div className='flex mt-3 md:text-2xl text-lg items-center'>
             <label className='mr-2'>C.I.</label>
-            <input className='text-black text-xl p-2 bg-white rounded-md w-[290px] sm:w-[302px]' type='text' value={user.CI} disabled/>
+            <input className='text-black text-xl p-2 bg-white rounded-md w-full ' type='text' value={user.CI} disabled/>
         </div>
         <div className='flex mt-3 md:text-2xl text-lg items-center'>
             <label className='mr-2'>Teléfono</label>
-            <input className='text-black text-xl p-2 bg-white rounded-md sm:w-[243px] w-[247px]' type='text' value={user.phone} disabled/>
+            <input className='text-black text-xl p-2 bg-white rounded-md w-full' type='text' value={user.phone} disabled/>
         </div>
         {
             role === 'athlete'
@@ -33,7 +33,7 @@ function LeftSide({user, role}) {
             <div className='flex mt-3 md:text-2xl text-lg items-center'>
                 <label className='mr-2'>Balance</label>
                 <input 
-                className='text-black text-xl p-2 bg-white rounded-md sm:w-[243px] w-[247px]' 
+                className='text-black text-xl p-2 bg-white rounded-md w-full' 
                 type='text' 
                 value={(user.info.deposited_money - user.info.payed_money) / 100 + ' CUP'} 
                 disabled/>
@@ -45,16 +45,16 @@ function LeftSide({user, role}) {
             <div className='flex mt-3 md:text-2xl text-lg items-center'>
                 <label className='mr-2'>Ganancia</label>
                 <input 
-                className='text-black text-xl p-2 bg-white rounded-md sm:w-[228px] w-[232px]' 
+                className='text-black text-xl p-2 bg-white rounded-md w-full' 
                 type='text' 
                 value={(user.info.amount_earned - user.info.amount_charged) / 100 + ' CUP'} 
                 disabled/>
             </div>
         }
         <div className='mt-5'>
-            <Link href={`/user/${user.CI}/edit`} className='text-xl block w-[320px] sm:w-[350px] bg-white text-black py-2 rounded-lg text-center'>Actualizar datos</Link>
-            <Link href='/' className='text-xl block w-[320px] sm:w-[350px] bg-white text-black py-2 rounded-lg text-center mt-5'>Cambiar contraseña</Link>
-            <Link href='/' className='text-xl block w-[320px] sm:w-[350px] bg-white text-black py-2 rounded-lg text-center mt-5'>Registros</Link>
+            <Link href={`/user/${user.CI}/edit`} className='text-xl block w-full bg-white text-black py-2 rounded-lg text-center'>Actualizar datos</Link>
+            <Link href='/' className='text-xl block w-full bg-white text-black py-2 rounded-lg text-center mt-5'>Cambiar contraseña</Link>
+            <Link href='/' className='text-xl block w-full bg-white text-black py-2 rounded-lg text-center mt-5'>Registros</Link>
         </div>
     </div>
     </>
